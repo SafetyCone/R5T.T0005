f3f5e3459c5ccd3969f7ec7f1e6686404ca9dce6
MinexAutomation <minex.automation@gmail.com>
2020-06-11 12:06:28 -0700

    Added GeneralXElement type.

---
+++ source/R5T.T0005/Code/Classes/GeneralXElement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Xml.Linq;
+
+
+namespace R5T.T0005
+{
+    /// <summary>
+    /// A general XElement type for use there is no specific XElement type.
+    /// </summary>
+    public class GeneralXElement : TypedXElement
+    {
+        public GeneralXElement(XElement value)
+            : base(value)
+        {
+        }
+    }
+}
---
+++ source/R5T.T0005/Code/Extensions/XElementExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Xml.Linq;
+
+
+namespace R5T.T0005
+{
+    public static class XElementExtensions
+    {
+        public static GeneralXElement AsGeneralXElement(this XElement xElement)
+        {
+            var generalElement = new GeneralXElement(xElement);
+            return generalElement;
+        }
+    }
+}
