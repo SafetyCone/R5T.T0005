004735e30dcf133f1336fba103835ceb18152545
MinexAutomation <minex.automation@gmail.com>
2020-06-11 11:01:34 -0700

    Adding TypedXElement type and extensions.

---
+++ source/R5T.T0005/Code/Extensions/TypedXElementExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Xml.Linq;
+
+using R5T.Magyar.Xml;
+
+
+namespace R5T.T0005
+{
+    public static class TypedXElementExtensions
+    {
+        public static string GetChildValue(this TypedXElement typedXElement, string childName)
+        {
+            var output = typedXElement.Value.GetChildValue(childName);
+            return output;
+        }
+
+        public static bool HasChild(this TypedXElement typedXElement, string childName, out XElement child)
+        {
+            var hasChild = typedXElement.Value.HasChild(childName, out child);
+            return hasChild;
+        }
+    }
+}
